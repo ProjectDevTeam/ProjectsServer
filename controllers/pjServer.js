affd57dcb77a7e81ac66f2393e6b5ae87836b6a9
wangliuyuan <wangliuyuanperson@163.com>
2022-06-01 18:16:30 +0800

    modify:1、更新SQL

--- controllers/pjServer.js
+++ controllers/pjServer.js
@@ -6,17 +6,36 @@ const pjServer = {
 		fun: (req, res, next) => {
 			const { cookies } = req;
 			const cookie = cookies['X-Request-Auth'];
-			const sql = `SELECT id,userName,theme FROM userInfo WHERE cookie='${cookie}'`;
+			const sql = `SELECT id,userName,theme, routes FROM userInfo WHERE cookie='${cookie}'`;
 			conn.query(sql, (err, data, flieds) => {
 				if (err)
 					res.status(200).json({
 						code: 300,
 						messages: [err]
 					});
-				res.status(200).json({
-					code: 0,
-					messages: ['success'],
-					data: data[0]
+				const resData = { ...data[0] };
+				const routes = resData.routes.split(',');
+				let whereSql = '';
+				routes.forEach((item, index) => {
+					if (index == 0) {
+						whereSql += `id=${item}`;
+					} else {
+						whereSql += ` or id=${item}`;
+					}
+				});
+				const routesSql = `select * from \`t-routes\` where ${whereSql}`;
+				conn.query(routesSql, '', (err, routeData) => {
+					if (err)
+						res.status(200).json({
+							code: 300,
+							messages: [err]
+						});
+					resData.routes = routeData;
+					res.status(200).json({
+						code: 0,
+						messages: ['success'],
+						data: resData
+					});
 				});
 			});
 		},
--- projectsMysql.sql
+++ projectsMysql.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 31/05/2022 18:10:31
+ Date: 01/06/2022 18:15:40
 */
 
 SET NAMES utf8mb4;
@@ -36,6 +36,30 @@ INSERT INTO `sysConfig` (`config`, `data`, `exp`) VALUES ('theme', '0,1', 'dark,
 COMMIT;
 
 -- ----------------------------
+-- Table structure for t-routes
+-- ----------------------------
+DROP TABLE IF EXISTS `t-routes`;
+CREATE TABLE `t-routes` (
+  `id` int NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `children` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of t-routes
+-- ----------------------------
+BEGIN;
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (1, '/home', '主页', NULL);
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (2, '/themetool', '主题设计', NULL);
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (3, '/speedtest', '网速平台', NULL);
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (4, '/charts', '报表平台', NULL);
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (5, '/acerdior', '在线编辑器', NULL);
+INSERT INTO `t-routes` (`id`, `path`, `name`, `children`) VALUES (6, '/photoset', '相册平台', NULL);
+COMMIT;
+
+-- ----------------------------
 -- Table structure for userInfo
 -- ----------------------------
 DROP TABLE IF EXISTS `userInfo`;
@@ -46,6 +70,7 @@ CREATE TABLE `userInfo` (
   `cookie` varchar(255) DEFAULT NULL,
   `endTime` varchar(255) DEFAULT NULL,
   `theme` int DEFAULT NULL,
+  `routes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -53,7 +78,7 @@ CREATE TABLE `userInfo` (
 -- Records of userInfo
 -- ----------------------------
 BEGIN;
-INSERT INTO `userInfo` (`id`, `userName`, `password`, `cookie`, `endTime`, `theme`) VALUES ('01', 'admin', 'admin', '1653963062587995551107', '1653967213831', 1);
+INSERT INTO `userInfo` (`id`, `userName`, `password`, `cookie`, `endTime`, `theme`, `routes`) VALUES ('01', 'admin', 'admin', '165406283015444536996', '1654067571281', 1, '1,2,3,4,5,6');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
